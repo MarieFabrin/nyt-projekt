d5d2163ae586fb8f9d4e9da4811689007d016602
Lilli-H <lilhut01@easv365.dk>
2025-12-16 17:17:51 +0100

    no review mobile but pop up

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ function sendsvar() {
         alert('Besked skal udfyldes');
     }
 }
+
+// Get the modal
+var modal = document.getElementById("popUp");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("popUpX")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
